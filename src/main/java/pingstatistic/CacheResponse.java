4c04a8e96d0f45e338d9d2517cc31fa9ddaee6a0
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 19:32:06 +0300

    Scripted auto-commit on change (2021-12-17 19:32:06) by gitwatch.sh

--- src/main/java/pingstatistic/CacheResponse.java
+++ src/main/java/pingstatistic/CacheResponse.java
@@ -1,10 +1,10 @@
 package pingstatistic;
 
-
+public class CacheResponse {
     private final String url;
     private final Long time;
 
-    public String getUrl() {
+
         return url;
     }
 
