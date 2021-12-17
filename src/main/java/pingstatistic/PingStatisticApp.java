b55c9f68dea352cc678e966ce552bfe5501e1344
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 18:38:59 +0300

    Scripted auto-commit on change (2021-12-17 18:38:59) by gitwatch.sh

--- src/main/java/pingstatistic/PingStatisticApp.java
+++ src/main/java/pingstatistic/PingStatisticApp.java
@@ -27,7 +27,7 @@ import java.util.concurrent.Future;
 public class PingStatisticApp {
     public static final int SERVER_PORT = 8080;
     public static final int OK_CODE = 200;
-
+    public static ActorRef cache;
 
     public static final String DEFAULT_URL = "https://www.google.ru/";
 
