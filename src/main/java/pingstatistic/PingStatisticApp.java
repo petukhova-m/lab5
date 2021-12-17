ac145207cdade8bdd5b3b134f9dcc728996e24ff
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 18:41:23 +0300

    Scripted auto-commit on change (2021-12-17 18:41:23) by gitwatch.sh

--- src/main/java/pingstatistic/PingStatisticApp.java
+++ src/main/java/pingstatistic/PingStatisticApp.java
@@ -58,7 +58,7 @@ public class PingStatisticApp {
                                                     pair.first(),
                                                     sum/pair.second()
 
-
+                                )
                             );
                 });
     }
