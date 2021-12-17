8769c3694789dbb519aa970e406e07b9b03149e6
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 19:33:03 +0300

    Scripted auto-commit on change (2021-12-17 19:33:03) by gitwatch.sh

--- src/main/java/pingstatistic/PingStatisticApp.java
+++ src/main/java/pingstatistic/PingStatisticApp.java
@@ -92,7 +92,7 @@ public class PingStatisticApp {
                 .map(PingStatisticApp::makePair)
                 .mapAsync(5, PingStatisticApp::asy)
                 .map(result -> {
-                    Pair<String, Long> pair = (Pair<String, Long>) result;
+
                     cache.tell(new StoreRequest(pair.first(), pair.second()), ActorRef.noSender());
                     System.out.printf("url: %s ping: %d", pair.first(), pair.second());
                     return HttpResponse.create().withStatus(OK_CODE).withEntity(pair.second().toString());
