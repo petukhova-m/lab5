3bc352fbca473a7a25b641104cb61df0621d01c8
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 19:07:43 +0300

    Scripted auto-commit on change (2021-12-17 19:07:43) by gitwatch.sh

--- src/main/java/pingstatistic/CacheActor.java
+++ src/main/java/pingstatistic/CacheActor.java
@@ -21,11 +21,11 @@ public class CacheActor extends AbstractActor{
     }
 
 
-        String url = request.first();
+
 
                 new CacheResponse(
                         url,
-                        data.containsKey(url) ? data.get(url) : -1L
+
                 ),
                 getSelf()
         );
