0651eedfade25346fc3898b53223370ad8662062
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 19:25:03 +0300

    Scripted auto-commit on change (2021-12-17 19:25:03) by gitwatch.sh

--- src/main/java/pingstatistic/CacheActor.java
+++ src/main/java/pingstatistic/CacheActor.java
@@ -12,7 +12,7 @@ public class CacheActor extends AbstractActor{
     public Receive createReceive() {
 
 
-                .match(StoreRequest.class, this::storeToCache)
+
                 .build();
     }
 
