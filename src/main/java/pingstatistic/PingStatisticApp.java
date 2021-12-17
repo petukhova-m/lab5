b95927cd118e4df1c44b82b3bc3babdf5ebf1fdb
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 18:59:37 +0300

    Scripted auto-commit on change (2021-12-17 18:59:37) by gitwatch.sh

--- src/main/java/pingstatistic/PingStatisticApp.java
+++ src/main/java/pingstatistic/PingStatisticApp.java
@@ -16,7 +16,7 @@ import org.asynchttpclient.AsyncHttpClient;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Collections;
+
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
