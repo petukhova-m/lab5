f5f4fa0ddfb26720a13710a4ee155b536021d73c
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 18:12:49 +0300

    Scripted auto-commit on change (2021-12-17 18:12:49) by gitwatch.sh

---
+++ src/main/java/pingstatistic/CacheActor.java
@@ -0,0 +1,35 @@
+package pingstatistic;
+
+
+
+import java.util.HashMap;
+
+
+public class CacheActor extends AbstractActor{
+
+
+
+
+        return ReceiveBuilder.create()
+
+
+
+
+
+
+
+
+
+
+        String url = request.first();
+
+
+
+
+
+
+
+
+
+
+}
