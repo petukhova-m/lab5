5eafae804bc42eb1e9cd74bf8b9521d58768e270
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 19:15:04 +0300

    Scripted auto-commit on change (2021-12-17 19:15:04) by gitwatch.sh

--- src/main/java/pingstatistic/PingStatisticApp.java
+++ src/main/java/pingstatistic/PingStatisticApp.java
@@ -97,7 +97,7 @@ public class PingStatisticApp {
                     System.out.printf("url: %s ping: %d", pair.first(), pair.second());
                     return HttpResponse.create().withStatus(OK_CODE).withEntity(pair.second().toString());
                 });
-
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", SERVER_PORT),
                 materializer
