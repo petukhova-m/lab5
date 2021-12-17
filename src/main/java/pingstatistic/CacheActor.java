affc9870b0288bbc7b5fc133328e7eee26b8f40e
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 19:24:00 +0300

    Scripted auto-commit on change (2021-12-17 19:23:59) by gitwatch.sh

--- src/main/java/pingstatistic/CacheActor.java
+++ src/main/java/pingstatistic/CacheActor.java
@@ -3,7 +3,7 @@ import akka.actor.AbstractActor;
 import akka.japi.Pair;
 import akka.japi.pf.ReceiveBuilder;
 import java.util.HashMap;
-
+import java.util.Map;
 
 public class CacheActor extends AbstractActor{
     private final Map<String, Long> data = new HashMap<>();
