d306fe49118dbcf1bc1fdd0c17c0a1de9a20084e
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 19:29:22 +0300

    Scripted auto-commit on change (2021-12-17 19:29:22) by gitwatch.sh

--- src/main/java/pingstatistic/PingStatisticApp.java
+++ src/main/java/pingstatistic/PingStatisticApp.java
@@ -62,7 +62,7 @@ public class PingStatisticApp {
                             );
                 });
     }
-
+    private static Sink<Pair<String, Integer>, CompletionStage<Long>> testSink() {
         return Flow
                 .<Pair<String, Integer>>create()
                 .mapConcat(request -> Collections.nCopies(request.second(), request.first()))
